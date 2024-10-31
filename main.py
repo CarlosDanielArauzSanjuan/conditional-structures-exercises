4716718535f55c9aa8b0a9349c4bd036e34847be
CarlosDanielArauzSanjuan <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2024-10-31 11:34:20 -0500

    Exercise6 "character"

--- main.py
+++ main.py
@@ -1,54 +1,25 @@
-#Escriba un programa que reciba como entrada dos números, y los muestre ordenados de menor a mayor:
-#
-#Ingrese numero: 51
-#Ingrese numero: 24
-#24 51
-#A continuación, escriba otro programa que haga lo mismo con tres números:
+#Escriba un programa que determine si un caracter ingresado es letra, número, o ninguno de los dos. 
+# En caso que sea letra, determine si es mayúscula o minúscula.
 
-#Ingrese numero: 8
-#Ingrese numero: 1
-#Ingrese numero: 4
-#1 4 8
-#Finalmente, escriba un tercer programa que ordene cuatro números:
+#Ingrese caracter: 9
+#Es numero.
+#Ingrese caracter: A
+#Es letra mayúscula.
+#Ingrese caracter: f
+#Es letra minúscula.
+#Ingrese caracter: #
+#No es letra ni número.#
 
-#Ingrese numero: 7
-#Ingrese numero: 0
-#Ingrese numero: 6
-#Ingrese numero: 1
-# 1 6 7
-#Recuerde que su programa debe entregar la solución correcta para cualquier combinación de números, no sólo para los ejemplos mostrados aquí.
 
-#Hay más de una manera de resolver cada ejercici#o.
+character = input ("insert a single character: ")
 
-#Programa para ordenar dos números:
+if character.isdigit():
+    print ("Is a number")
 
-number1 = int(input("insert number: "))
-number2 = int(input("insert number: "))
-
-if number1 > number2:
-    print(number2, number1)
+elif character.isalpha(): 
+    if character.isupper():
+        print ("the letter is odded")
+    else:
+        print ("the letter is lowercase")
 else:
-    print(number1, number2)
-
-#print("Exercise 5 sort numbers")
-
-number1 = int(input("insert number: "))
-number2 = int(input("insert number: "))
-number3 = int(input("insert number: "))
-
-numbers = [number1 ,number2, number3]
-numbers.sort()
-
-print(numbers)
-
-#print("Exercise 5 sort numbers")
-
-n1 = int(input("insert number: "))
-n2 = int(input("insert number: "))
-n3 = int(input("insert number: "))
-n4 = int(input("insert number: "))
-
-numbers = [n1 ,n2, n3, n4]
-numbers.sort()
-
-print(numbers)
+    print ("It is neither a letter nor a number")
