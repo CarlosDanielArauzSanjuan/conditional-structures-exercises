fce6c55caea4fa3564db2dc2fbb8fbccc451142c
CarlosDanielArauzSanjuan <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2024-10-31 11:06:25 -0500

    Exercise3

--- main.py
+++ main.py
@@ -13,21 +13,18 @@
 #Cociente: 10
 #Resto: 0
 
-# Solicitar al usuario que ingrese el dividendo y el divisor
+
 div = int(input("Dividends: "))
 div2 = int(input("Divider: "))
 
-# Calcular el cociente y el resto
 quotient = div // div2
 reminder = div % div2
 
-# Verificar si la división es exacta
+
 if reminder == 0:
     print("La división es exacta.")
 else:
     print("La división no es exacta.")
-
-# Mostrar el cociente y el resto
 print(f"quotient: {quotient}")
 print(f"Reminder: {reminder}")
 
