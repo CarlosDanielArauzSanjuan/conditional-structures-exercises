0ce86ee7c428ca85f873cc703e528563cb981d68
CarlosDanielArauzSanjuan <186082333+CarlosDanielArauzSanjuan@users.noreply.github.com>
2024-10-31 11:43:08 -0500

    Exercise10 "triangles"

--- main.py
+++ main.py
@@ -1,42 +1,33 @@
-#El joven periodista Solarrabietas debe relatar un partido de tenis, pero no conoce las reglas del deporte. En particular, no ha logrado aprender cómo saber si un set ya terminó, y quién lo ganó.
+#Los tres lados a, b y c de un triángulo deben satisfacer la desigualdad triangular: cada uno de los lados no puede ser más largo que la suma de los otros dos.
 
-#Un partido de tenis se divide en sets. Para ganar un set, un jugador debe ganar 6 juegos, pero además debe haber ganado por lo menos dos juegos más que su rival. Si el set está empatado a 5 juegos, el ganador es el primero que llegue a 7. Si el set está empatado a 6 juegos, el set se define en un último juego, en cuyo caso el resultado final es 7-6.
+#Escriba un programa que reciba como entrada los tres lados de un triángulo, e indique:
 
-#Sabiendo que el jugador A ha ganado m juegos, y el jugador B, n juegos, al periodista le gustaría saber:
+#si acaso el triángulo es inválido; y
+#si no lo es, qué tipo de triángulo es.
+#Ingrese a: 3.9
+#Ingrese b: 6.0
+#Ingrese c: 1.2
+#No es un triangulo valido.
+#Ingrese a: 1.9
+#Ingrese b: 2
+#Ingrese c: 2
+#El triangulo es isoceles.
+#Ingrese a: 3.0
+#Ingrese b: 5.0
+#Ingrese c: 4.0
+#El triangulo es escaleno.
 
-#si A ganó el set, o
-#si B ganó el set, o
-#si el set todavía no termina, o
-#si el resultado es inválido (por ejemplo, 8-6 o 7-3).
-#Desarrolle un programa que solucione el problema de Solarrabietas:
 
-#Juegos ganados por A: 4
-#Juegos ganados por B: 5
-#Aun no termina
-#Juegos ganados por A: 5
-#Juegos ganados por B: 7
-#Gano B
-#Juegos ganados por A: 5
-#Juegos ganados por B: 6
-#Aun no termina
-#Juegos ganados por A: 3
-#Juegos ganados por B: 7
-#Invalido
-#Juegos ganados por A: 6
-#Juegos ganados por B: 4
-#Gano A
-p1 = int(input("games won by A: "))
-p2 = int(input("games won by B: "))
+t1 = float(input("Enter side A: "))
+t2 = float(input("Enter side B: "))
+t3 = float(input("Enter side C: "))
 
-#IF ITS INVALID
-if p1 > 7 or p2 > 7 or ( p1==7 and p2 < 5) or (p2==7 and p1 < 5) or abs(p1-p2) > 2:
-    print ("Invalid")
-#IF ITS NOT FINISHED
-elif (p1 <= 6 and p2 <= 6 ) or (p1==6 and p2==6):
-    print ("It's not over yet")
-#IF P1 WON
-elif (p1 == 6 and p2 <= 4) or (p1 == 7 and p2 == 5) or (p1 == 7 and p2 == 6):
-    print ("Won A")
-
-elif (p2 == 6 and p1 <= 4) or (p2 == 7 and p1 == 5) or (p2 == 7 and p1 == 6):
-    print ("Won B")
+if t1 + t2 > t3 and t1 + t3 > t2 and t2 + t3 > t1:
+    if  t1 == t2 == t3:
+        print ("triangle is equilateral")
+    elif t1 == t2 or t1 == t3 or t2 == t3:
+        print ("triangle is isosceles")
+    else:
+        print ("triangle is scalene")
+else:
+    print ("its no a valid triangle ")
